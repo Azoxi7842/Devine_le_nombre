9f70a0d06df71a5bcf4404f96e3b9c04624bb7dc
Azoxi7842 <arthuros12346@gmail.com>
2024-07-26 19:45:39 +0200

    Ajout
    Ajout du fichier du mini - jeux

---
+++ Devine_le_nombre_fr.py
@@ -0,0 +1,24 @@
+import random
+
+
+secret_number = random.randint(1, 100)
+
+print("Bienvenue au jeu de devinettes!")
+print("Devinez le nombre entre 1 et 100")
+
+attempts = 0
+
+while True:
+   
+    guess = int(input("Entrez votre devinette: "))
+    attempts += 1
+
+    if guess == secret_number:
+        print(f"Félicitations! Vous avez deviné le bon nombre en {attempts} tentatives.")
+        break
+    elif guess < secret_number:
+        print("Trop bas! Essayez encore.")
+    else:
+        print("Trop haut! Essayez encore.")
+
+        
